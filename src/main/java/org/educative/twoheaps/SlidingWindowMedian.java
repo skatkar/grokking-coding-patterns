c17320743c6a579273dbdf58e30d99baeff370db
skatkar <skatkar225@gmail.com>
2023-07-04 16:49:30 -0500

    Two heaps

--- src/main/java/org/educative/twoheaps/SlidingWindowMedian.java
+++ src/main/java/org/educative/twoheaps/SlidingWindowMedian.java
@@ -1,59 +1,72 @@
 package org.educative.twoheaps;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 class Solution {
-    PriorityQueue<Integer> minHeap;
+
     PriorityQueue<Integer> maxHeap;
+    PriorityQueue<Integer> minHeap;
+
+    public Solution() {
+        initializeHeaps();
+    }
+
+    private void initializeHeaps() {
+        this.minHeap = new PriorityQueue<>();
+        this.maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+    }
+
+    private void insertNum(int num) {
+        // always try to maintain the size of maxHeap >= minHeap by 1
+        if(maxHeap.isEmpty() || maxHeap.peek() > num) {
+            maxHeap.add(num);
+        }else
+            minHeap.add(num);
+
+        reBalance();
+    }
+
+    // The below methods are worst performing and would cause TLE on leetcode
+    // Try to find out a way to handle this in O(1).
+    // Possibly putting in a map and deleting only if the top of the heaps contain any of the number in map.
+    private void remove(int num) {
+        if(num > maxHeap.peek()) {
+            minHeap.remove(num);
+        }
+        else
+            maxHeap.remove(num);
+        reBalance();
+    }
+
+    private void reBalance() {
+        // time to re-balance the heaps
+        if(maxHeap.size() > minHeap.size() + 1) {
+            minHeap.add(maxHeap.poll());
+        }else if(maxHeap.size() < minHeap.size())
+            maxHeap.add(minHeap.poll());
+    }
+
+    // TC : O(1)
+    // SC : O(1)
+    public double findMedian() {
+        if(maxHeap.size() == minHeap.size()) {
+            return minHeap.peek() / 2.0 + maxHeap.peek() / 2.0;
+        }
+        return maxHeap.peek();
+    }
+
     public double[] medianSlidingWindow(int[] nums, int k) {
-        if(nums == null || nums.length == 0) return new double[]{};
-
-        List<Double> result = new ArrayList<>();
-        minHeap = new PriorityQueue<>((a,b) -> Integer.compare(a,b));
-        maxHeap = new PriorityQueue<>((a,b)->(b.compareTo(a)));
-
+        double[] result = new double[nums.length - k + 1];
         for(int i=0; i < nums.length; i++) {
-            // 1. Add number to the correct heap
-            if(maxHeap.isEmpty() || nums[i] <= maxHeap.peek()) {
-                maxHeap.add(nums[i]);
-            }else
-                minHeap.add(nums[i]);
-
-            // 2. re-balance the heap as we added a number to the heap
-            rebalance();
-
-            // 3. We reached the window limit, let's calculate the median
+            insertNum(nums[i]);
+            // = condition will work only once and later all the indexes will use > condition
             if(i + 1 >= k) {
-                if(maxHeap.size() == minHeap.size()) {
-                    result.add(minHeap.peek() / 2.0 + maxHeap.peek() / 2.0);
-                }else{
-                    result.add(maxHeap.peek() * 1.0);
-                }
-
-                // 3.1. Reduce the window size by 1
-                int outgoing = nums[i - k + 1];
-
-                // the below methods are worst performing and would cause TLE on leetcode
-                // Try to find out a way to handle this in O(1).
-                // Possibly putting in a map and deleting only if the top of the heaps contain any of the number in map.
-                if(outgoing <= maxHeap.peek()) maxHeap.remove(outgoing);
-                else minHeap.remove(outgoing);
-
-                rebalance();
+                result[i-k+1] = findMedian();
+                remove(nums[i-k+1]);
             }
         }
-
-        return result.stream()
-                .mapToDouble(Double::doubleValue)
-                .toArray();
-    }
-
-    private void rebalance() {
-        if(maxHeap.size() < minHeap.size()) {
-            maxHeap.add(minHeap.poll());
-        }else if(maxHeap.size() - minHeap.size() > 1)
-            minHeap.add(maxHeap.poll());
+        return result;
     }
 }
+
