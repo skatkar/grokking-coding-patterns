1bb0806379a006ad857349a3d21ef1ffd79e4fc7
skatkar <skatkar225@gmail.com>
2023-08-10 23:09:36 -0500

    Recursion

---
+++ src/main/java/org/educative/recursion/BinarySearch.java
@@ -0,0 +1,29 @@
+package org.educative.recursion;
+
+public class BinarySearch {
+    public static boolean binarySearch(int[] nums, int target) {
+        if(nums == null || nums.length == 0) return false;
+
+        return searchNumber(nums, target, 0, nums.length - 1);
+    }
+
+    private static boolean searchNumber(int[] nums, int target, int low, int high) {
+        if(low > high)
+            return false;
+
+        int mid = low + (high - low) / 2;
+
+        if(nums[mid] == target)
+            return true;
+        else if(target < nums[mid])
+            return searchNumber(nums, target, low, mid - 1);
+        else
+            return searchNumber(nums, target, mid + 1, high);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(BinarySearch.binarySearch(new int[]{1, 2, 3, 4, 5}, 4));
+        System.out.println(BinarySearch.binarySearch(new int[]{2, 4, 6, 8, 10}, 5));
+        System.out.println(BinarySearch.binarySearch(new int[]{3, 6, 9, 12, 15}, 15));
+    }
+}
---
+++ src/main/java/org/educative/recursion/NumberFreq.java
@@ -0,0 +1,20 @@
+package org.educative.recursion;
+
+public class NumberFreq {
+    public static int countOccurrences(int[] arr, int key) {
+        if(arr == null || arr.length == 0)
+            return 0;
+        return helper(arr, key, 0);
+    }
+
+    private static int helper(int[] arr, int key, int index) {
+        if(index == arr.length)
+            return 0;
+        return arr[index] == key ? 1 + helper(arr, key, index + 1) : helper(arr,key, index + 1);
+    }
+
+    public static void main(String[] args) {
+        int i = NumberFreq.countOccurrences(new int[]{1, 2, 2, 2, 3}, 2);
+        System.out.println("i = " + i);
+    }
+}
