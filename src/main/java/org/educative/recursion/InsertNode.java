44c49e02fd55899872e927d94a4f5b144a52312f
skatkar <skatkar225@gmail.com>
2023-08-11 21:22:51 -0500

    Recursion

---
+++ src/main/java/org/educative/recursion/InsertNode.java
@@ -0,0 +1,14 @@
+package org.educative.recursion;
+
+public class InsertNode {
+    public TreeNode insert(TreeNode root, int value) {
+        if(root == null)
+            return new TreeNode(value);
+
+        if(value < root.val)
+            root.left = insert(root.left, value);
+        else if(value > root.val)
+            root.right = insert(root.right, value);
+        return root;
+    }
+}
