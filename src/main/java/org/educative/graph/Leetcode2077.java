5c3b16e6067f240345ecc5ab13991f4904136be4
skatkar <skatkar225@gmail.com>
2023-09-16 18:00:27 -0500

    Graph problems - practice

--- src/main/java/org/educative/graph/Leetcode2077.java
+++ src/main/java/org/educative/graph/Leetcode2077.java
@@ -7,38 +7,49 @@ import java.util.Set;
 
 public class Leetcode2077 {
     // TC : O(E) -> number of edges in the graph, corridor
-    // SC : O(V^2) -> V = vertices in the graph, number of rooms
+    // SC : O(E)
+    Map<Integer, Set<Integer>> map;
     public int numberOfPaths(int n, int[][] corridors) {
-        int cycles = 0;
-        Map<Integer, Set<Integer>> map = new HashMap<>();
+        map = new HashMap<>();
 
+        buildGraph(corridors);
+
+        int count = 0;
+        for(int[] corridor : corridors) {
+            int room1 = corridor[0];
+            int room2 = corridor[1];
+
+            Set<Integer> room1Neighbors = map.get(room1);
+            Set<Integer> room2Neighbors = map.get(room2);
+
+            // Find the intersection
+            if(room1Neighbors != null && room2Neighbors != null)
+                count += room1Neighbors.stream().filter(room2Neighbors::contains).count();
+        }
+
+        return count;
+    }
+
+    private void buildGraph(int[][] corridors){
         for(int[] corridor : corridors) {
             int room1 = corridor[0];
             int room2 = corridor[1];
 
             // Initialize the room and it's neighbors' set
-            map.putIfAbsent(room1, new HashSet<>());
-            map.putIfAbsent(room2, new HashSet<>());
-
-            map.get(room1).add(room2);
-            map.get(room2).add(room1);
-
-            // Now we know that room1 and room2 are connected to each other
-            // we just need to find whether is there any other node which is common among them
-            // If yes, then it forms a path of 3 nodes -> confusion score
-
-            cycles += intersectionPaths(map.get(room1), map.get(room2));
+            // The if else conditions mentioned below will make the undirected graph as directed graph
+            // So, there will be an edge from smaller node to larger node and not the other way
+            // This will avoid the duplicate cycles
+            // E,g, If the nodes are 1,3,4 (assuming all of them are connected)
+            // Possible cycles are : 1 -> 3 -> 4 -> 1, 4 -> 1 -> 3 -> 4, 3 -> 4 -> 1 -> 3
+            // The below logic will get only the first cycle and will ignore the other two.
+            if (room1 < room2){
+                map.putIfAbsent(room1, new HashSet<>());
+                map.get(room1).add(room2);
+            }
+            else {
+                map.putIfAbsent(room2, new HashSet<>());
+                map.get(room2).add(room1);
+            }
         }
-
-        return cycles;
-    }
-
-    private int intersectionPaths(Set<Integer> set1, Set<Integer> set2) {
-        int count = 0;
-        for(int node : set1) {
-            if(set2.contains(node))
-                count++;
-        }
-        return count;
     }
 }
