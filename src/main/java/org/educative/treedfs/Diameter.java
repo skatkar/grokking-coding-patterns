cb71238aac31172e88528f921410001a85ea8b29
Swapnil Katkar <skatkar225@gmail.com>
2023-12-07 22:31:49 -0600

    Tree diameter

--- src/main/java/org/educative/treedfs/Diameter.java
+++ src/main/java/org/educative/treedfs/Diameter.java
@@ -4,19 +4,36 @@ package org.educative.treedfs;
  * Leetcode 543
  */
 public class Diameter {
-    public int diameterOfBinaryTree(TreeNode root) {
-        int treeDiameter = 0;
-        findDiameter(root, treeDiameter);
-        return treeDiameter;
+    public static void main(String[] args) {
+        Diameter diameter = new Diameter();
+        TreeNode root = new TreeNode(1);
+        TreeNode firstLeft = new TreeNode(2);
+        TreeNode firstRight = new TreeNode(3);
+        TreeNode secondLeft = new TreeNode(4);
+        TreeNode secondRight = new TreeNode(5);
+
+        root.left = firstLeft;
+        root.right = firstRight;
+        firstLeft.left = secondLeft;
+        firstLeft.right = secondRight;
+
+        int answer = diameter.diameterOfBinaryTree(root);
+        System.out.println("answer = " + answer);
     }
 
-    private int findDiameter(TreeNode root, int treeDiameter) {
+    public int diameterOfBinaryTree(TreeNode root) {
+        int[] diameter = new int[1];
+        findDiameter(root, diameter);
+        return diameter[0];
+    }
+
+    private int findDiameter(TreeNode root, int[] treeDiameter) {
         if(root == null) return 0;
 
         int left = findDiameter(root.left, treeDiameter);
         int right = findDiameter(root.right, treeDiameter);
 
-        treeDiameter = Math.max(treeDiameter, left + right);
+        treeDiameter[0] = Math.max(treeDiameter[0], left + right);
 
         return 1 + Math.max(left, right);
     }
