fda5f08a4949c087fb5e5c317caf1b10ff24b991
Swapnil Katkar <skatkar225@gmail.com>
2024-01-07 15:08:48 -0600

    Leetcode39

---
+++ src/main/java/org/educative/subsets/Leetcode1498.java
@@ -0,0 +1,35 @@
+package org.educative.subsets;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Leetcode1498 {
+    public static void main(String[] args) {
+        Leetcode1498 q = new Leetcode1498();
+        int numSubseq = q.numSubseq(new int[]{2,3,3,4,6,7}, 12);
+        System.out.println("numSubseq = " + numSubseq);
+    }
+
+    public int numSubseq(int[] nums, int target) {
+        Arrays.sort(nums);
+        return (int) (generateSubset(0, nums, new ArrayList<>(),target) % (Math.pow(10,9) + 7));
+
+    }
+
+    private long generateSubset(int index, int[] nums, List<Integer> subset, int target) {
+        if(index >= nums.length - 1 ){
+            if(!subset.isEmpty() && subset.get(0) + subset.get(subset.size() - 1) <= target)
+                return 1;
+            return 0;
+        }
+
+        subset.add(nums[index]);
+        long left = generateSubset(index + 1, nums, subset, target);
+
+        subset.remove(subset.size() - 1);
+        long right = generateSubset(index + 1, nums, subset, target);
+
+        return left + right;
+    }
+}
---
+++ src/main/java/org/educative/subsets/Leetcode39.java
@@ -0,0 +1,43 @@
+package org.educative.subsets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode39 {
+    public static void main(String[] args) {
+        Leetcode39 q = new Leetcode39();
+        List<List<Integer>> lists = q.combinationSum(new int[]{2, 3, 6, 7}, 7);
+        System.out.println("lists = " + lists);
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> subsets = new ArrayList<>();
+        generateCombinations(0, candidates, new ArrayList<Integer>(), subsets, target);
+        return subsets;
+    }
+
+    private void generateCombinations(int index, int[] candidates, List<Integer> subset, List<List<Integer>> subsets, int target) {
+        if(index == candidates.length){
+            if(target == 0){
+                subsets.add(new ArrayList<>(subset));
+            }
+            return;
+        }
+
+        // If the number is greater than the target, then we don't have to start the recursion
+        // otherwise it will go in an infinite loop.
+
+        // since we are allowed to choose the number as many times as possible,
+        // we need some terminating condition else infinite loop.
+        if(candidates[index] <= target){
+            subset.add(candidates[index]);
+            target -= candidates[index];
+            generateCombinations(index, candidates, subset, subsets, target);
+
+            subset.remove(subset.size() - 1);
+            target += candidates[index];
+        }
+
+        generateCombinations(index + 1, candidates, subset, subsets, target);
+    }
+}
