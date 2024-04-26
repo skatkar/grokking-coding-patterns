ac6e7f2043f99a25ccd72fc7b95c750b4e02b199
Swapnil Katkar <skatkar225@gmail.com>
2024-04-25 23:09:51 -0700

    Combination similar questions

---
+++ src/main/java/org/educative/subsets/practice/Leetcode46.java
@@ -0,0 +1,36 @@
+package org.educative.subsets.practice;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Leetcode46 {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        generatePermutations(nums, new HashSet<>(), new ArrayList<Integer>(), result);
+        return result;
+    }
+
+    private void generatePermutations(int[] nums, Set<Integer> visited, List<Integer> sublist, List<List<Integer>> result) {
+        if(sublist.size() == nums.length){
+            result.add(new ArrayList<>(sublist));
+            return;
+        }
+
+        for(int num : nums) {
+            if(!visited.contains(num)){
+                sublist.add(num);
+                visited.add(num);
+
+                generatePermutations(nums, visited, sublist, result);
+
+                sublist.remove(sublist.size() - 1);
+                visited.remove(num);
+            }
+        }
+    }
+
+
+}
