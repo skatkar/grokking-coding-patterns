7e43d5ba39e550891f89d8de32b577b70458a5a7
Swapnil Katkar <skatkar225@gmail.com>
2024-06-21 22:00:51 -0700

    DP

--- src/main/java/org/educative/graph/concepts/TopoSort.java
+++ src/main/java/org/educative/graph/concepts/TopoSort.java
@@ -3,16 +3,56 @@ package org.educative.graph.concepts;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Stack;
 
 public class TopoSort {
 
+
     /**
+     * Pick a node, DFS its neighbors and once all the neighbors are visited, put it in a stack.
+     * Using DFS
+     * @param V
+     * @param adj
+     * @return
+     */
+    public int[] topoSort(int V, List<List<Integer>> adj) {
+        boolean[] visited = new boolean[V];
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[V];
+        int index = 0;
+
+        for(int i=0; i < V; i++) {
+            if(!visited[i]){
+                dfs(i, stack, adj, visited);
+            }
+        }
+
+        while(!stack.isEmpty()) {
+            int current = stack.pop();
+            result[index++] = current;
+        }
+
+        return result;
+    }
+
+    private void dfs(int node, Stack<Integer> stack, List<List<Integer>> adj, boolean[] visited) {
+        visited[node] = true;
+        for(int neighbor : adj.get(node)) {
+            if(!visited[neighbor]) {
+                dfs(neighbor, stack, adj, visited);
+            }
+        }
+        stack.push(node);
+    }
+
+    /**
+     * Kahn's algorithm
      * Topologically sorted vertices
      * @param V - Number of vertices
      * @param adj - Adjacency list
      * @return sorted array
      */
-    public int[] topoSort(int V, List<List<Integer>> adj) {
+    public int[] topoSort_BFS(int V, List<List<Integer>> adj) {
         int[] inDegrees = new int[V];
         int[] sorted = new int[V];
         int pointer = 0;
