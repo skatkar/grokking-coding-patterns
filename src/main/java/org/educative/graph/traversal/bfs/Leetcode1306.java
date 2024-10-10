54b4cb626c7089f931e8805d505b54b619e4087b
Swapnil Katkar <skatkar225@gmail.com>
2024-10-09 22:01:49 -0700

    Graph BFS traversals

--- src/main/java/org/educative/graph/traversal/bfs/Leetcode1306.java
+++ src/main/java/org/educative/graph/traversal/bfs/Leetcode1306.java
@@ -8,7 +8,7 @@ import java.util.Queue;
 public class Leetcode1306 {
     public static void main(String[] args) {
         Leetcode1306 q = new Leetcode1306();
-        boolean canReach = q.canReach2(new int[]{58,48,64,36,19,19,67,13,32,2,59,50,29,68,50,0,69,31,54,20,22,43,30,9,68,71,20,22,48,74,2,65,27,54,30,5,66,24,64,68,9,31,50,59,15,72,6,49,11,71,12,61,5,66,30,1,2,39,59,35,53,21,76,17,71,40,68,57,64,53,70,21,50,49,25,63,35}, 46);
+        boolean canReach = q.canReach(new int[]{58,48,64,36,19,19,67,13,32,2,59,50,29,68,50,0,69,31,54,20,22,43,30,9,68,71,20,22,48,74,2,65,27,54,30,5,66,24,64,68,9,31,50,59,15,72,6,49,11,71,12,61,5,66,30,1,2,39,59,35,53,21,76,17,71,40,68,57,64,53,70,21,50,49,25,63,35}, 46);
         System.out.println("canReach = " + canReach);
     }
 
@@ -58,11 +58,13 @@ public class Leetcode1306 {
 
         while(!queue.isEmpty()){
             int curr = queue.poll();
+            System.out.println("curr = " + curr + " visited " + visited[curr]);
             if(arr[curr] == 0)
                 return true;
             for(int neighbor : adj.get(curr)) {
                 if(!visited[neighbor]){
                     queue.add(neighbor);
+                    visited[neighbor] = true;
                 }
             }
         }
