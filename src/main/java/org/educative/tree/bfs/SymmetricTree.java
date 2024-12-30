4dd5ed48ec8debf864974f41362b46373d62e93e
Swapnil Katkar <skatkar225@gmail.com>
2024-12-29 18:23:35 -0800

    Tree questions

--- src/main/java/org/educative/tree/bfs/SymmetricTree.java
+++ src/main/java/org/educative/tree/bfs/SymmetricTree.java
@@ -33,4 +33,18 @@ public class SymmetricTree {
 
         return true;
     }
+
+    public boolean isSymmetric_DFS(TreeNode root) {
+        if(root == null) return true;
+
+        return helper(root.left, root.right);
+    }
+
+    private boolean helper(TreeNode left, TreeNode right) {
+        if(left == null && right == null) return true;
+
+        if(left == null || right == null || left.val != right.val) return false;
+
+        return helper(left.left, right.right) && helper(left.right, right.left);
+    }
 }
