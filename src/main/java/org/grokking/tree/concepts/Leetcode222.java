1fcf98e98ae1e7a5cbab904b8602c44c34a6ffe7
Swapnil Katkar <skatkar225@gmail.com>
2024-12-31 14:54:55 -0800

    Tree questions

---
+++ src/main/java/org/grokking/tree/concepts/Leetcode222.java
@@ -0,0 +1,41 @@
+package org.grokking.tree.concepts;
+
+import org.grokking.tree.bfs.TreeNode;
+
+public class Leetcode222 {
+    // Explanation - https://www.youtube.com/watch?v=u-yWemKGWO0&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=33
+    // TC : O(log n ^ 2)
+    // SC : O(log n)
+    public int countNodes(TreeNode root) {
+        if(root == null) return 0;
+
+        int leftHeight = getLeftHeight(root);
+        int rightHeight = getRightHeight(root);
+
+        if(leftHeight == rightHeight)
+            return (2 << leftHeight) - 1; // Math.pow(2, leftHeight) - 1
+        else
+            return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+
+    private int getLeftHeight(TreeNode root){
+        int count = 0;
+        while(root.left != null){
+            count++;
+            root = root.left;
+        }
+
+        return count;
+    }
+
+    private int getRightHeight(TreeNode root){
+        int count = 0;
+        while(root.right != null){
+            count++;
+            root = root.right;
+        }
+
+        return count;
+    }
+
+}
