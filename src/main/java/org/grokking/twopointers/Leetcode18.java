ee43b868e6e6144216fb2e3964aa34550f2bbf40
Swapnil Katkar <skatkar225@gmail.com>
2025-01-28 15:17:23 -0800

    4Sum

--- src/main/java/org/grokking/twopointers/Leetcode18.java
+++ src/main/java/org/grokking/twopointers/Leetcode18.java
@@ -10,42 +10,42 @@ public class Leetcode18 {
         return kSum(nums, 0, 4, target);
     }
 
-    private List<List<Integer>> kSum(int[] nums, int start, int k, int target) {
-        List<List<Integer>> result = new ArrayList<>();
+    // Make the target as null. One of the test cases was failing for int data type
+    private List<List<Integer>> kSum(int[] nums, int start, int k, long target) {
         int len = nums.length;
-        if(k == 2){
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if (k == 2) { // two pointers from left and right
             int left = start, right = len - 1;
-
-            while(left < right) {
+            while (left < right) {
                 int sum = nums[left] + nums[right];
-                if(sum == target){
-                    List<Integer> path = new ArrayList<>();
+                if (sum == target) {
+                    List<Integer> path = new ArrayList<Integer>();
                     path.add(nums[left]);
                     path.add(nums[right]);
-                    result.add(path);
-
-                    while(left < right && nums[left] == nums[left + 1]) left++;
-                    while(left < right && nums[right] == nums[right - 1]) right--;
-
+                    res.add(path);
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right - 1])
+                        right--;
                     left++;
                     right--;
-                } else if (sum < target) {
+                } else if (sum < target) { // move left
                     left++;
-                }else{
+                } else { // move right
                     right--;
                 }
             }
-        }else{
-            for(int i=start; i < len - (k - 1); i++) {
-                if(i > start && nums[i] == nums[i - 1])
+        } else {
+            for (int i = start; i < len - (k - 1); i++) {
+                if (i > start && nums[i] == nums[i - 1])
                     continue;
-                List<List<Integer>> lists = kSum(nums, i + 1, k - 1, target - nums[i]);
-                for(List<Integer> subList : lists) {
-                    subList.add(0, nums[i]);
+                List<List<Integer>> temp = kSum(nums, i + 1, k - 1, target - nums[i]);
+                for (List<Integer> t : temp) {
+                    t.add(0, nums[i]);
                 }
-                result.addAll(lists);
+                res.addAll(temp);
             }
         }
-        return result;
+        return res;
     }
 }
